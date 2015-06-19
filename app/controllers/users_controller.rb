0298909d3fed7dd34b78a5d9f3c993a89f04fa78
Duncan Neal <duncan.neal@gmail.com>
2015-06-19 14:05:51 -0400

    users, broken avatars, n stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+ 
+  if @user.save
+    redirect_to @user, notice: "Thank you for signing up for Twixter!"
+  else
+    render 'new'
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email_address, :avatar, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   devise_for :users
+  resources :users
   resources :twixts
+
   
   root 'twixts#index'
+ 
 end
