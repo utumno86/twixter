4b320038dfbfd793eb4a4565d6db0c05b32bd297
Michael E. Nash <utumno86@gmail.com>
2015-06-19 10:35:11 -0400

    added index, pagination, and twixt controller methods

--- app/controllers/twixts_controller.rb
+++ app/controllers/twixts_controller.rb
@@ -1,8 +1,52 @@
 class TwixtsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: [:index]
+
   def index
-    
+    @twixts = Twixt.all.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @twixt = current_user.twixt.build
+  end
+
+  def edit
+  end
+
+  def create
+    @twixt = current_user.twixt.build(twixt_params)
+
+    if @twixt.save
+      redirect_to @twixt, notice: 'Twixt was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @twixt.update(twixt_params)
+      redirect_to @twixt, notice: 'Twixt was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @twixt.destroy
+    redirect_to twixts_url, notice: 'Twixt was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_twixt
+    @twixt = Twixt.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def twixt_params
+    params.require(:twixt).permit(:content)
   end
   
 end
