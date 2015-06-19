1387ea0043e64330b9ef7046af09267de544d1b2
Michael E. Nash <utumno86@gmail.com>
2015-06-19 14:45:11 -0400

    fixed the carrierwave config file and unlessed out the avatar line

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:                'us-east-1'
   }
-  config.fog_directory  = Rails.env.development? ? 'twixt-dev' : 'twixt'
+  config.fog_directory  = Rails.env.development? ? ENV['AWS_BUCKET_NAME'] : 'twixt'
   config.fog_public     = false
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
 end
