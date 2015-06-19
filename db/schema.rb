83fdaa646c1e486b6f7c0f9952ca823db5f878cf
Duncan Neal <duncan.neal@gmail.com>
2015-06-19 10:35:13 -0400

    Merged.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,15 @@ ActiveRecord::Schema.define(version: 20150619142344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "twixts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "twixts", ["user_id"], name: "index_twixts_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -35,4 +44,5 @@ ActiveRecord::Schema.define(version: 20150619142344) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "twixts", "users"
 end
