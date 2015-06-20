aa023ed03d6608a62eb97b9ed3d666055ed79faf
Duncan Neal <duncan.neal@gmail.com>
2015-06-20 19:43:12 -0400

    User tweek in preps for following feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @twixt = Twixt.new
   end
 
   def create
--- app/models/twixt.rb
+++ app/models/twixt.rb
@@ -1,3 +1,6 @@
 class Twixt < ActiveRecord::Base
+  attr_accessible :content, :userid
   belongs_to :user
+ 
+  validates :content, length: { maximum: 140 }
 end
