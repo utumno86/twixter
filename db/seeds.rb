a8ee65c0cfcad9bbf495bdceebbe0f7c3ded29c8
Michael E. Nash <utumno86@gmail.com>
2015-06-21 16:34:58 -0400

    added populate scripts to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+30.times do
+  u = User.new
+  u.name = Faker::Name.name
+  u.email = Faker::Internet.email(u.name)
+  u.password = Faker::Internet.password(8)
+  u.password_confirmation = u.password
+  u.avatar = Faker::Avatar.image
+  u.save
+end
+
+30.times do
+  t = Twixt.new
+  t.user_id = Random.rand(1..User.count)
+  t.content = Faker::Hacker.say_something_smart
+  t.save
+end
