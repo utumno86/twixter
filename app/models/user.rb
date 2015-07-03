70440d2b13f1a8900bacea2a3ab7426f97318609
Aaron Harvey <harvnlenny@gmail.com>
2015-07-03 12:52:00 -0400

    started omniauth with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
   
   has_many :twixts, dependent: :destroy
 
@@ -26,6 +27,12 @@ class User < ActiveRecord::Base
       u.save
     end
 
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    user
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users, :controllers =>{
-                 :registrations => 'registrations'
+                 :registrations => 'registrations',
+                 :omniauth_callbacks => "users/omniauth_callbacks"
                    }
   
   resources :users
