a2126045641c27a7fcda8d0e9207ee744332e787
cateia97 <bristolkr@gmail.com>
2015-07-03 16:12:31 -0400

    Omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,12 @@ class User < ActiveRecord::Base
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
+    unless user
+        user = User.create(name: data["name"],
+           email: data["email"],
+           password: Devise.friendly_token[0,20]
+        )
+    end
     user
   end
 end
